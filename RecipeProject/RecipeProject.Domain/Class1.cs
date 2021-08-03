ce35770c8c4bf170472f91ea5f3bf6abe13cc1f6
ingrid fleury <ingridfleury1@gmail.com>
2021-08-03 23:46:19 +0100

    Implementation of the the project structure and folders.

---
+++ RecipeProject/RecipeProject.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace RecipeProject.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ RecipeProject/RecipeProject.Domain/Model/Recipe.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecipeProject.Domain.Model
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+    }
+}
