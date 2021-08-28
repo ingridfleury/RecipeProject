57bac56ff13e3b68a194828483252dd5f6241649
ingrid fleury <ingridfleury1@gmail.com>
2021-08-28 23:33:48 +0100

    Dificulty enum implementation and contructor creation.

--- RecipeProject/RecipeProject.Domain/Model/Dificulty.cs
+++ RecipeProject/RecipeProject.Domain/Model/Dificulty.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using RecipeProject.Domain.Model.Enums;
 
 namespace RecipeProject.Domain.Model
 {
     public class Dificulty
     {
+
         public int DificultyId { get; set; }
+        public DificultyOption DificultyDefinition { get; set; }
+
+        public Dificulty(int dificultyId, DificultyOption dificultyDefinition)
+        {
+            DificultyId = dificultyId;
+            DificultyDefinition = dificultyDefinition;
+        }
 
     }
 }
---
+++ RecipeProject/RecipeProject.Domain/Model/Enums/DificultyOption.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecipeProject.Domain.Model.Enums
+{
+    public enum DificultyOption
+    {
+        Fácil,
+        Médio,
+        Difícil
+    }
+}
