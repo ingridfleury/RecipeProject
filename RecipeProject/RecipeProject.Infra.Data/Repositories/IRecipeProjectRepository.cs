94af304bf308e08e4406acfdc7c02e96fa472392
ingrid fleury <ingridfleury1@gmail.com>
2021-09-09 17:41:55 +0100

    RecipeProject methods implementation

--- RecipeProject/RecipeProject.Infra.Data/Repositories/IRecipeProjectRepository.cs
+++ RecipeProject/RecipeProject.Infra.Data/Repositories/IRecipeProjectRepository.cs
@@ -1,10 +1,46 @@
-﻿using System;
+﻿using RecipeProject.Domain.Model;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace RecipeProject.Infra.Data.Repositories
 {
-    public interface IRecipeProjectRepository 
+    public class RecipeProjectRepository 
     {
+        private readonly DataBase context;
+        public RecipeProjectRepository(DataBase context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<Recipe> Get()
+        {
+            return context.Recipes.ToList();
+        }
+        public Recipe Create(Recipe recipe)
+        {
+            context.Recipes.Add(recipe);
+            context.SaveChanges();
+            return recipe;
+        }
+
+        public Recipe GetById(int id)
+        {
+            return context.Recipes.FirstOrDefault(x => x.RecipeId == id);
+        }
+
+        public Recipe Update(Recipe recipe)
+        {
+            context.Recipes.Update(recipe);
+            context.SaveChanges();
+            return recipe;
+        }
+        public Recipe Delete(Recipe recipe)
+        {
+            context.Recipes.Remove(recipe);
+            context.SaveChanges();
+            return recipe;
+        }
     }
 }
