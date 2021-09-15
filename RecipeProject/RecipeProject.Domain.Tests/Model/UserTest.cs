6f1373f237549caac888020991a74b5e47371f9e
ingrid fleury <ingridfleury1@gmail.com>
2021-09-16 00:45:11 +0100

    Implementing test with password hash and salt, on user.

--- RecipeProject/RecipeProject.Domain.Tests/Model/UserTest.cs
+++ RecipeProject/RecipeProject.Domain.Tests/Model/UserTest.cs
@@ -1,6 +1,31 @@
-﻿namespace RecipeProject.Domain.Tests.Model
+﻿using RecipeProject.Domain.Model;
+using Xunit;
+
+namespace RecipeProject.Domain.Tests.Model
 {
-    class UserTest
+    public class UserTest
     {
+        private User _sut;
+
+        [Fact]
+        public void PasswordSalt_Equal_to_PasswordHash_Should_Throw() 
+            // *: existe uma convenção para a definição do nome do método de teste?
+        {
+            //Arrange
+            byte[] PasswordHash = null;  // *: qual tipo da variável devo utilizar para o teste?
+            byte[] PasswordSalt = null;
+
+            //Act
+            _sut = new User();
+
+            //Assert
+
+            Assert.Equal(PasswordSalt, PasswordHash);
+            // *: O retorno deste assert será boolean?
+
+
+
+        }
+
     }
 }
