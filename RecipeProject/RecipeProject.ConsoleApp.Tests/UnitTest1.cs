e7e59e86d0479b51f53fd84a7f1e314ee6b3d10f
ingrid fleury <ingridfleury1@gmail.com>
2021-09-15 20:38:21 +0100

    Tests projects creation in each project.

---
+++ RecipeProject/RecipeProject.ConsoleApp.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace RecipeProject.ConsoleApp.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
